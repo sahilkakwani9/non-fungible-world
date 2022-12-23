cd0e6c175e564057d2ccc9bfc1a5236afbeabe04
sahilkakwani9 <sahkakwani@gmail.com>
2022-12-23 17:17:23 +0530

    fea: nftcard

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,33 +1,44 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { use, useState } from 'react'
+import { NFTCard } from '../components/nftCard'
+import fetchCollection from '../utils/fetchCollections'
 import fetchNFTs from '../utils/fetchNFTs'
 
 const Home = () => {
   const [wallet, setWallet] = useState('');
   const [collection, setCollection] = useState('');
   const [NFT, setNFT] = useState([]);
+  const [filterbyCollection, setFilterbyCollection] = useState(false);
   return (
     <div className="flex flex-col items-center justify-center py-8 gap-y-3">
-    <div className="flex flex-col w-full justify-center items-center gap-y-2">
-      <input type={"text"} onChange={(e)=>{setWallet(e.target.value)}} value={wallet} placeholder="Add your wallet address"></input>
-      <input type={"text"} onChange={(e)=>{setCollection(e.target.value)}} value={collection} placeholder="Add the collection address"></input>
-      <label className="text-gray-600 "><input type={"checkbox"} className="mr-2"></input>Fetch for collection</label>
+      <div>
+        <h1 className='text-6xl text-blue-500 font-extrabold'>Non-Fungible World</h1>
+      </div>
+    <div className="flex w-full justify-around items-center gap-y-2 mt-8">
+      <input type={"text"} className="border-2 border-gray-600 rounded-md px-4" disabled={filterbyCollection} onChange={(e)=>{setWallet(e.target.value)}} value={wallet} placeholder="Add your wallet address"></input>
+      <input type={"text"} className="border-2 border-gray-600 rounded-md px-4 w-60" onChange={(e)=>{setCollection(e.target.value)}} value={collection} placeholder="Add the collection address"></input>
+      <label className="text-gray-600 "><input type={"checkbox"} className="mr-2" onChange={(e)=>setFilterbyCollection(e.target.checked)}></input>Fetch for collection</label>
         <button onClick={async()=>{
-          setNFT(await fetchNFTs(wallet, collection));
+          if(filterbyCollection){
+            setNFT(await fetchCollection(collection));
+          }
+          else{
+            setNFT(await fetchNFTs(wallet, collection));
+          }
           console.log(NFT);
 
-        }} className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"}>Let's go! </button>
+        }} className={"disabled:bg-slate-500 text-white bg-blue-400 py-2 text-lg rounded-lg px-12"}>Get NFT's! </button>
     </div>
-    <div>
-      <h2>{wallet}</h2>
-      <h2>{collection}</h2>
-    </div>
-    {
-      NFT.map((n,i)=>{
-        return <h1>Sahil</h1>
-      })
-    }
+    <div className='flex flex-wrap gap-y-12 mt-4 w-8/9 gap-x-8 justify-center'>
+        {
+          NFT && NFT.map(nft => {
+            return (
+              <NFTCard nft={nft}></NFTCard>
+            )
+          })
+        }
+      </div>
   </div>
   )
 }
---
+++ utils/fetchCollections.js
@@ -0,0 +1,17 @@
+const fetchCollection = async (collection) => {
+    if (collection.length) {
+      var requestOptions = {
+        method: 'GET'
+      };
+      const api_key = "A8A1Oo_UTB9IN5oNHfAc2tAxdR4UVwfM"
+      const baseURL = `https://eth-mainnet.g.alchemy.com/v2/${api_key}/getNFTsForCollection/`;
+      const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}`;
+      const nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
+      if (nfts) {
+        console.log("NFTs in collection:", nfts)
+        return nfts.nfts
+      }
+    }
+  }
+
+export default fetchCollection;
